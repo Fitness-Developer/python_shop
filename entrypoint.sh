f7c548b7eab9592997b29b6e29e4d1c6c77455be
Alexandr <mastertri97@gmail.com>
2025-03-14 12:43:21 +0300

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/local/bin/redis-server &
-/usr/sbin/rabbitmq-server &
+/opt/rabbitmq/sbin/rabbitmq-server &
 
 sleep 10
 
-/project/daphne -b 0.0.0.0 -p 8000 project.asgi:application
+daphne -b 0.0.0.0 -p 8000 project.asgi:application
